666838476dd6cd706f411cbc96af7fc2c306c3f2
noknow <noknow@noknownoMacBook-ea.local>
2019-08-01 10:57:10 +0900

    Modified few bugs

--- crypto.py
+++ crypto.py
@@ -20,6 +20,28 @@
 #         # When Encryption for CBC mode
 #         encrypted = crypto.EncryptCBC(key, plainText)
 #     
+#
+#
+# MIT License
+#
+# Copyright (c) 2019 noknow.info
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ######################################################################
 import binascii
 import math
@@ -29,7 +51,10 @@ from Crypto import Random
 from Crypto.Util import Counter
 
 class Crypto:
-    
+
+    ##################################################
+    # Constructor
+    ##################################################    
     def __init__(self):
         self.vMajor = sys.version_info.major
         self.vMinor = sys.version_info.minor
